9aa3475d00444b7534b5729205f8403524efa862
YomoABH <yomoyomosai@gmail.com>
2025-04-01 17:47:13 +0300

    refactor: Добавить комментарий

--- src/api.ts
+++ src/api.ts
@@ -3,6 +3,7 @@ import { createSocket } from "./lib/SocketManager"
 export const $socket = createSocket(
    "wss://s14395.nyc1.piesocket.com/v3/1?api_key=wxLxmHNRQ8a5OXRbyUtd0jga6RSZU3cxcEcXA8SB&notify_self=1"
 )
+// по хорошему тут надо делать иначе, но перебор для одного запроса был бы
 export async function getChuckNorrisJoke(): Promise<string> {
    try {
       const response = await fetch("https://api.chucknorris.io/jokes/random")
